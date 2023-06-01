30d31c727658768e4704494cf7867baa5d19b75e
RishikantSri <>
2023-06-01 15:52:39 +0530

    Email functionality mail() added

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -9,35 +9,53 @@ use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
- 
-                public function storeContactForm(Request $request)
-                {
-                // dd('testing..');
-                    $validator = Validator::make($request->all(), [
-                        'name' => 'required',
-                        'email' => 'required|email',
-                        'subject' => 'required',
-                        'message' => 'required',
-                        
-                    ]);
 
-                
-                    if (!$validator->passes()){
+    public function storeContactForm(Request $request)
+    {
+        // dd('testing..');
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'message' => 'required',
 
-                        return response()->json(['error'=>$validator->errors()->all()]); 
-                    }
-                    
-                        $contact = new Contact();
-                        $contact->name = $request->name;
-                        $contact->email = $request->email;
-                        $contact->phone = $request->phone;
-                        $contact->subject = $request->subject;
-                        $contact->message = $request->message;
-                        $contact->attachement = $input['attachement'] ?? 'default.jpeg';
-                        $contact->save();
+        ]);
 
-                        return response()->json(['success'=>'Your message has been sent. We will contact you asap. Thank you!']);
-                }
 
-                    
+        if (!$validator->passes()) {
+
+            return response()->json(['error' => $validator->errors()->all()]);
+        }
+
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->subject = $request->subject;
+        $contact->message = $request->message;
+        $contact->attachement = $input['attachement'] ?? 'default.jpeg';
+        $contact->save();
+
+
+        // sending email
+
+        $to = "xyz@somedomain.com";
+        $subject = "Admin: You Received New Contact Form's Query";
+
+        $message = "<b>This is HTML message.</b>";
+        $message .= "<h1>This is headline.</h1>";
+
+        $header = "From:abc@somedomain.com \r\n";
+        $header .= "Cc:afgh@somedomain.com \r\n";
+        $header .= "MIME-Version: 1.0\r\n";
+        $header .= "Content-type: text/html\r\n";
+
+        $retval = mail($to, $subject, $message, $header);
+
+        if ($retval == true) {
+            return response()->json(['success' => 'Your message has been sent. We will contact you asap. Thank you!']);
+        } else {
+            return response()->json(['error' => 'Please try after some time']);
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,44 +15,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('frontend.home');
-})->name('home');;
+    Route::get('/', function () {
+        return view('frontend.home');
+    })->name('home');;
 
-// Frontend Routes 
+    // Frontend Routes 
 
-Route::get('/about', function () {
-    return view('frontend.about');
-})->name('about');
+    Route::get('/about', function () {
+        return view('frontend.about');
+    })->name('about');
 
-Route::get('/services', function () {
-    return view('frontend.services');
-})->name('services');
+    Route::get('/services', function () {
+        return view('frontend.services');
+    })->name('services');
 
-Route::get('/projects', function () {
-    return view('frontend.projects');
-})->name('projects');
+    Route::get('/projects', function () {
+        return view('frontend.projects');
+    })->name('projects');
 
-Route::get('/contact', function () {
-    return view('frontend.contact');
-})->name('contact');
+    Route::get('/contact', function () {
+        return view('frontend.contact');
+    })->name('contact');
 
-Route::get('/features', function () {
-    return view('frontend.features');
-})->name('features');
+    Route::get('/features', function () {
+        return view('frontend.features');
+    })->name('features');
 
-Route::get('/team', function () {
-    return view('frontend.ourteam');
-})->name('team');
+    Route::get('/team', function () {
+        return view('frontend.ourteam');
+    })->name('team');
 
-Route::get('/faq', function () {
-    return view('frontend.faq');
-})->name('faq');
+    Route::get('/faq', function () {
+        return view('frontend.faq');
+    })->name('faq');
 
-Route::get('/testimonial', function () {
-    return view('frontend.testimonials');
-})->name('testimonial');
+    Route::get('/testimonial', function () {
+        return view('frontend.testimonials');
+    })->name('testimonial');
 
 
-Route::post('/contact-form', [App\Http\Controllers\Frontend\ContactController::class, 'storeContactForm'])->name('contact-form.store');
+    Route::post('/contact-form', [App\Http\Controllers\Frontend\ContactController::class, 'storeContactForm'])->name('contact-form.store');
 
